55fb0d3bb026b7db91cfff4ff2cf5d7c51c2ae73
Dmitriy Onypko <vaporif@gmail.com>
2022-12-24 02:06:45 +0000

    upd

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use sqlx::PgPool;
-use tracing_log::LogTracer;
 use std::net::TcpListener;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use zero2prod_rs::{configuration::get_configuration, startup::run};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    LogTracer::init().expect("logs were not attached"); 
+    LogTracer::init().expect("logs were not attached");
     let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
     let formatting_layer = BunyanFormattingLayer::new(
         "zero2prod".into(),
-        // Output the formatted spans to stdout. 
-        std::io::stdout
+        // Output the formatted spans to stdout.
+        std::io::stdout,
     );
     let subscriber = Registry::default()
         .with(env_filter)
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -21,9 +21,7 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> im
     );
 
     let _span_guard = request_span.enter();
-    let query_span = tracing::info_span!(
-        "Saving new subscriber details in the database"
-    );
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions(id, email, name, subscribed_at)
