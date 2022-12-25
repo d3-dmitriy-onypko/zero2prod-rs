f62a539a3b850b847f5b143e3b34939a76a7eb51
Dmytro Onypko <vaporif@gmail.com>
2022-12-25 00:37:18 +0000

    tracing (#3)
    * tracing
    
    * upd
    
    * upd
    
    * upd
    
    * upd
    
    * upd

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,7 +1,8 @@
 use actix_web::{web, HttpResponse, Responder};
 
 #[allow(unused, unused_imports)]
-use sqlx::{types::chrono::Utc, PgPool};
+use sqlx::PgPool;
+use tracing::Instrument;
 #[allow(unused, unused_imports)]
 use uuid::Uuid;
 
@@ -11,6 +12,16 @@ pub struct FormData {
     name: String,
 }
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name= %form.name
+    );
+
+    let _span_guard = request_span.enter();
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions(id, email, name, subscribed_at)
@@ -19,14 +30,22 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> im
         Uuid::new_v4(),
         form.email,
         form.name,
-        Utc::now()
+        sqlx::types::time::OffsetDateTime::now_utc()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => HttpResponse::Ok(),
+        Ok(_) => {
+            tracing::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id
+            );
+
+            HttpResponse::Ok()
+        }
         Err(e) => {
-            eprint!("Failed to execute query: {}", e);
+            tracing::error!("failed to execute query {:?}", e);
             HttpResponse::InternalServerError()
         }
     }
