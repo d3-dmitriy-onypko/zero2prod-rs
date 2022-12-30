143b0e558418ec21578919be0965fb16c6b1f76d
Dmitriy Onypko <vaporif@gmail.com>
2022-12-30 17:02:05 +0000

    upd

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,30 +13,31 @@ pub struct FormData {
 #[tracing::instrument(
  name = "Adding a new subscriber", skip(form, pool),
  fields(
-    subscriber_email = %form.email, 
+    subscriber_email = %form.email,
     subscriber_name= %form.name
  )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
     match insert_subscriber(&pool, &form).await {
         Ok(_) => HttpResponse::Ok(),
-        Err(_) => HttpResponse::InternalServerError()
+        Err(_) => HttpResponse::InternalServerError(),
     }
 }
 
 #[tracing::instrument(
-name = "Saving new subscriber details in the database", skip(form, pool))]
-pub async fn insert_subscriber(
-    pool: &PgPool,
-    form: &FormData,
-    ) -> Result<(), sqlx::Error> {
-    sqlx::query!( r#"
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
     VALUES ($1, $2, $3, $4)
-    "#, 
-      Uuid::new_v4(),
-      form.email, form.name, 
-      sqlx::types::time::OffsetDateTime::now_utc()
+    "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        sqlx::types::time::OffsetDateTime::now_utc()
     )
     .execute(pool)
     .await
